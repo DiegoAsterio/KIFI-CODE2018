30750817f0df9652c92f373f19d3c592e8ae8610
braulio <brlvldvlsmrtnz@gmail.com>
2018-03-01 19:47:21 +0100

    Funcion distancia

--- code/data.py
+++ code/data.py
@@ -3,3 +3,6 @@ from collections import namedtuple
 Problem = namedtuple('Map', ['rows', 'cols', 'fleet', 'bonus', 'steps', 'rides'])
 Position = namedtuple('Position', ['row', 'col'])
 Ride = namedtuple('Ride', ['ride_id', 'start', 'finish', 't_start', 't_finish'])
+
+def dist(pos1, pos2):
+    return abs(pos1.rows - pos2.rows) + abs(pos1.cols - pos2.cols)
