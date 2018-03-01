b6d34f2426a62f456269aad3817b5a07707a45af
Nacho Mas <nacho.cronos2@gmail.com>
2018-03-01 19:28:38 +0100

    Fixes `car.py`

--- code/car.py
+++ code/car.py
@@ -1,6 +1,6 @@
-class car:
-def __init__:
-    self.rides = []
-    self.target = (0,0)
-    self.hasARide = True
-    self.remainingSteps = 0
+class Car(object):
+    def __init__(self):
+        self.rides = []
+        self.target = (0,0)
+        self.hasARide = True
+        self.remainingSteps = 0
