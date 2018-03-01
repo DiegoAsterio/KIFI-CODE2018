79c311d539599b176049dc85b25785e48f11176c
braulio <brlvldvlsmrtnz@gmail.com>
2018-03-01 19:39:27 +0100

    algorritmo jeje

---
+++ code/algorithm.py
@@ -0,0 +1,18 @@
+LOOKAHEAD_RIDES = 3
+
+def algorithm(problem):
+    steps = problem.steps
+    cars = [ Car() for _ in range(problem.fleet) ]
+    rides = problem.rides
+    lookahead = LOOKAHEAD_RIDES * problem.fleet
+
+    for step in range(steps):
+        for car in cars:
+            if car.is_free():
+                ride = car.choose_ride(rides[:lookahead])
+                rides.remove(ride)
+            
+            car.tick()
+
+    for car in cars:
+        car.output()
