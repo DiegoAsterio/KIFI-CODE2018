19819914019ed3cf695b20b9d0e8f5dae8f1b7e0
Juan Carlos Criado Alamo <jccc01@correo.ugr.es>
2018-03-01 19:50:24 +0100

    vkjwbs

--- code/car.py
+++ code/car.py
@@ -6,3 +6,8 @@ class Car(object):
 
     def is_free(self):
         return not self.remainingSteps
+    
+    def get_score(self, ride, current_time, bonus):
+        distance = dist(ride.start, self.target)
+        waiting_time = max(ride.t_start - current_time, 0)
+        duration = ride.t_finish - ride.t_start
