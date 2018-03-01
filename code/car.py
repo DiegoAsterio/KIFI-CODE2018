91da4d0de714cf4feee20600b4a05fece1212c2e
DiegoAsterio <diegoasterio@correo.ugr.es>
2018-03-01 19:38:39 +0100

    getRides done

--- code/car.py
+++ code/car.py
@@ -6,3 +6,16 @@ class Car(object):
 
     def is_free(self):
         return not self.remainingSteps
+
+    def choose_ride(self,step,rides):
+        scores = [getScores(ride) :  ride in rides]
+        max = -1
+        pos = 0
+        for i in range(scores.size() ):
+            if max < scores[i]:
+                max = scores[i]
+                pos = i
+        return rides[pos]
+            
+        
+                  
