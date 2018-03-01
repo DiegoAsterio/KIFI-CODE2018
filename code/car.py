988f46db9ba875e698830dee42f87f8ad62453fd
Nacho Mas <nacho.cronos2@gmail.com>
2018-03-01 19:31:27 +0100

    Updates Car

--- code/car.py
+++ code/car.py
@@ -2,5 +2,7 @@ class Car(object):
     def __init__(self):
         self.rides = []
         self.target = (0,0)
-        self.hasARide = True
         self.remainingSteps = 0
+
+    def is_free(self):
+        return not self.remainingSteps
